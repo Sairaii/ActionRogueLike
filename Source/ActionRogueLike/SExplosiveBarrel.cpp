9dd71a5f909509af9e4916e17df023757ae45b90
Vincent Konganda <vincentkonganda@yahoo.com>
2023-12-21 12:17:40 +0800

    Lecture 6 - Debugging Tools

--- Source/ActionRogueLike/SExplosiveBarrel.cpp
+++ Source/ActionRogueLike/SExplosiveBarrel.cpp
@@ -3,6 +3,7 @@
 
 #include "SExplosiveBarrel.h"
 
+#include "DrawDebugHelpers.h"
 #include "Components/StaticMeshComponent.h"
 #include "PhysicsEngine/RadialForceComponent.h"
 
@@ -46,5 +47,12 @@ void ASExplosiveBarrel::PostInitializeComponents()
 void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	ForceComp->FireImpulse();
+
+	UE_LOG(LogTemp, Log, TEXT("On Actor Hit In Explosive Barrel."))
+
+	UE_LOG(LogTemp, Warning, TEXT("OtherActor: %s, at game time: %f"), *GetNameSafe(OtherActor), GetWorld()->TimeSeconds);
+
+	FString CombinedString = FString::Printf(TEXT("Hit at location: %s"), *Hit.ImpactPoint.ToString());
+	DrawDebugString(GetWorld(), Hit.ImpactPoint, CombinedString, nullptr, FColor::Green, 2.0f, true);
 }
 
